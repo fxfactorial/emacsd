8c169aa9d77975845d19aed15d188d4231c1640f
Edgar Aroutiounian <edgar.factorial@gmail.com>
2014-11-13 11:21:19 -0500

    Doing clean slate cleanup

--- elpa/bbdb-vcard-20131216.1632/bbdb-vcard-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "bbdb-vcard" "20131216.1632" "vCard import/export for BBDB"
-  '((bbdb "3.0"))
-  :url "http://github.com/vgeddes/bbdb-vcard" :keywords
-  '("data" "calendar" "mail" "news"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/tronesque-theme-20140922.256/tronesque-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "tronesque-theme" "20140922.256" "Color Theme based on Tron universe." 'nil :url "https://github.com/aurelienbottazini/tronesque")
