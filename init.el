52794369843da39b2df8046a28ff7dd34410e26f
Edgar Aroutiounian <edgar.factorial@gmail.com>
2014-11-17 19:40:31 -0500

    Refactoring of init, added some niceities like C-return, M-return

--- init.el
+++ init.el
@@ -1,32 +1,29 @@
-;;OS X specifics
+;; OS X specifics
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
-;;Kind of useful?
+;; I like it to have emacs open on right half of screen. 
 (when window-system
   (set-frame-position nil 675 0)
   (set-frame-size nil 80 49))
-;;(setq ns-auto-hide-menu-bar t)
 (global-set-key (kbd "<M-down>") 'forward-paragraph)
 (global-set-key (kbd "<M-up>") 'backward-paragraph)
-;;Not sure why but dialog box still locks up emacs on OSX. 
+;; Not sure why but dialog box still locks up emacs on OSX.
+;; in any case, I dislike dialog boxes. 
 (setq use-dialog-box nil)
-;;Opens up in fullscreen natively, not sure if I actually care about this. 
-;;(set-frame-parameter nil 'fullscreen 'fullboth)
 (setq user-full-name "Edgar Aroutiounian")
 (setq user-mail-address "edgar.factorial@gmail.com")
 
-;;Giving myself this helpful buffer, otherwise way to many damn key
-;;bindings to remember!
+;; Giving myself this helpful buffer, otherwise way to many damn key
+;; bindings to remember!
 ((lambda ()
    (with-temp-buffer 
      (insert-file-contents "~/.emacs.d/custom_scratch_message.txt")
      (setq initial-scratch-message (buffer-string)))))
 
-;;Fuck it, too much struggling with CEDET, doing it myself, basically
-;;turns on everything. FUCKING EL_GET MAKES ME DO THIS FIRST
+;; this took many, many hours to get working correctly. 
 (load-file "~/.emacs.d/cedet/cedet-devel-load.elc")
 
-;;El-get stuff to sync up most stuff
+;; El-get stuff to sync up most stuff
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
@@ -41,11 +38,8 @@
 ;; everything helm/company related, just handle it manually.
 (add-to-list 'load-path "~/.emacs.d/elpa/helm-20141008.2145")
 (add-to-list 'load-path "~/.emacs.d/elpa/helm-gtags-20141005.243")
-;;Provides really nice autocomplete for C headers.
-(add-to-list 'load-path "~/.emacs.d/elpa/company-c-headers-20140930.1102")
-(add-to-list 'load-path "~/.emacs.d/cedet/lisp/cedet")
 
-;; All Requires done early.
+;; All Requires done relatively early.
 (require 'password-cache)
 (require 'semantic/senator)
 (require 'semantic/ia)
@@ -96,7 +90,7 @@
  '(solarized-use-more-italic t)
  '(tool-bar-mode nil))
 
-;;Custom Functions
+;; Custom Functions
 (defun revert-all-buffers ()
   "Refreshes all open buffers from their respective files."
   (interactive)
@@ -142,11 +136,12 @@
 			 " -C /home/w4118/hmwk5-prog/flo-kernel"))
   (advice-add 'call-process :around #'my-call-process-hack))
 
-(define-global-minor-mode this-linum-mode linum-mode
-  (lambda ()
-    (when (not (memq major-mode
-                     (list 'doc-view-mode)))
-      (linum-mode))))
+;; Since not using linum-mode anymore, no sense to run this code. 
+;; (define-global-minor-mode this-linum-mode linum-mode
+;;   (lambda ()
+;;     (when (not (memq major-mode
+;;                      (list 'doc-view-mode)))
+;;       (linum-mode))))
 
 (defun linux-c-mode ()
   "C mode with adjusted defaults for use with the linux kernel."
@@ -157,10 +152,12 @@
   (setq c-basic-offset 8)
   (setq tab-width 8))
 
-(defun make-etags ()
-  "Execute shell command to build tags, but better options available"
-  (interactive)
-  (shell-command "find . -type f -iname \"\*.[chS]\" | xargs etags -a"))
+;; Not really needed anymore since using global for basically everything
+;; just keeping this for legacy interest. 
+;; (defun make-etags ()
+;;   "Execute shell command to build tags, but better options available"
+;;   (interactive)
+;;   (shell-command "find . -type f -iname \"\*.[chS]\" | xargs etags -a"))
 
 (defun my-org-defaults ()
   "Adds some common stuff I always use to an org mode text file"
@@ -178,7 +175,7 @@
 		  "#include <unistd.h>\n"
 		  "#include <ctype.h>\n"
 		  "#include <string.h>\n")))
-		  
+
 (defun os-vm ()
   (interactive)
   (find-file "/ssh:os:~/hmwk5-prog/flo-kernel/mm/expose.c")
@@ -210,7 +207,7 @@
           (if this-win-2nd (other-window 1))))))
 
 (defadvice push-mark
-  (around semantic-mru-bookmark activate)
+    (around semantic-mru-bookmark activate)
   "Push a mark at LOCATION with NOMSG and ACTIVATE passed to `push-mark’.
    If `semantic-mru-bookmark-mode’ is active, also push a tag
    onto the mru bookmark stack."
@@ -229,15 +226,16 @@
 	(setq first (cdr (car (cdr alist)))))
     (semantic-mrub-switch-tags first)))
 
-;;Misc things
+;; Misc things
 (global-set-key (kbd "C-c C-g") 'google-this-noconfirm)
+;; Love ido, idiot for not using it earlier. 
 (setq ido-everywhere t)
 (ido-mode 1)
-;;Use the path set up by zsh, aka the ~/.zshrc. 
+;; Use the path set up by zsh, aka the ~/.zshrc. 
 (exec-path-from-shell-initialize)
-;;Annoying issue with TRAMP constantly asking for password
+;; Annoying issue with TRAMP constantly asking for password
 (setq password-cache-expiry nil)
-;;Keep the history between sessions
+;; Keep the history between sessions, very nice to have.
 (savehist-mode 1)
 (global-set-key (kbd "M-/") 'company-complete)
 ;; Just kill the shell, don't ask me.
@@ -249,12 +247,15 @@
 ;; Don't prompt me when I want to clear the buffer
 (put 'erase-buffer 'disabled nil)
 
-;;Visuals, but note that some visuals also set in custom.
+;; Visuals, but note that some visuals also set in custom.
 (abbrev-mode -1)
 (display-battery-mode 1)
 (electric-indent-mode 1)
 (electric-pair-mode 1)
-;;Using this wrapper because linum-mode fucks up doc-view mode. 
+;; I used to use linum-mode but its not really worth it, more like
+;; a crutch for beginning programmers, besides I see it in the mode line.
+;; in any case, have to use this wrapper function cause linum-mode
+;; fucks up doc-view, just in case you ever want to go back. 
 ;; (this-linum-mode 1)
 (setq inhibit-startup-message t)
 (window-number-mode)
@@ -266,20 +267,27 @@
 	     "~/.emacs.d/elpa/tronesque-theme-20140922.256")
 (fringe-mode 10)
 (tool-bar-mode -1)
+;; Default for emacs jumps like crazy, this is the sane answer. 
 (setq scroll-step 1)
-;;Gives me the full name of the buffer, hate just having foo.c
+;; Gives me the full name of the buffer, hate just having foo.c
 (add-hook 'find-file-hooks
 	  '(lambda ()
 	     (setq mode-line-buffer-identification 'buffer-file-truename)))
-;;Since not using line numbers, show me end of the buffer in the fringe
-(setq default-indicate-empty-lines t)
-;;Show me the trailing whitespace
+;; Since not using line numbers, show me end of the buffer in the fringe
+(setq-default indicate-empty-lines t)
+;; Obviously only for two buffers
 (global-set-key (kbd "C-'") 'toggle-window-split)
+;; Just for cycling through
+(global-set-key (kbd "<C-return>") 'next-buffer)
+;; Native full screen
+(global-set-key (kbd "<M-return>") 'toggle-frame-fullscreen)
+;; I hate this (its the list-buffer), always mistakenly call it and never want it.
+(global-unset-key (kbd "C-x C-b"))
 (when window-system
-;;  (load-theme 'solarized-dark))
-;;My other favorite theme. 
- (load-theme 'tronesque)
- (tronesque-mode-line))
+  (load-theme 'solarized-dark))
+;; My other favorite theme. 
+;; (load-theme 'tronesque)
+;; (tronesque-mode-line))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -288,7 +296,7 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:height 110 :family "Monaco")))))
 
-;;Stuff for gnus, want to use gmail, see ~/.gnus
+;; Stuff for gnus, want to use gmail, see ~/.gnus
 (add-hook 'message-mode-hook (lambda ()
 			       (load-file "~/.gnus")
 			       (company-mode)
@@ -297,7 +305,9 @@
 			       (local-set-key "<TAB>" 'bbdb-complete-name)
 			       (turn-on-auto-fill)))
 
-;;Semantic Stuff, very important to me.
+;; Semantic Stuff, very important to me, should probably refactor this for
+;; the appropriate modes, eitherwise the globalness of it is annoying when
+;; doing say Python and C, or rather anything else and C. 
 (global-semantic-idle-scheduler-mode 1)
 (global-semantic-decoration-mode 1)
 (global-semantic-idle-summary-mode 1)
@@ -308,83 +318,91 @@
 (global-cedet-m3-minor-mode 1)
 (semanticdb-enable-gnu-global-databases 'c-mode t)
 (global-semantic-show-unmatched-syntax-mode t)
-;;For custom stuff, be see this example
- ;; (ede-cpp-root-project "NAME" :file "FILENAME"
- ;;     :include-path '( "/include" "../include" "/c/include" )
- ;;     :system-include-path '( "/usr/include/c++/3.2.2/" )
- ;;     :spp-table '( ("MOOSE" . "")
- ;;                   ("CONST" . "const") )
- ;;     :spp-files '( "include/config.h" )
+;; For custom stuff, see this example (Although I skip this crap and just do the stuff
+;; in os-s)
+;; (ede-cpp-root-project "NAME" :file "FILENAME"
+;;     :include-path '( "/include" "../include" "/c/include" )
+;;     :system-include-path '( "/usr/include/c++/3.2.2/" )
+;;     :spp-table '( ("MOOSE" . "")
+;;                   ("CONST" . "const") )
+;;     :spp-files '( "include/config.h" )
 ;;     )
-;;If you do clang first, then you get awesome signatures in the
-;;popup for the functions. On the other hand, clang doesn't know
-;;the contenxt and so -> for structs doesn't give completion.
-;;with clang first as well, you can't do C-w to go to definition,
-;;but that's okay since you can do it with semantic anyway with M-]/[
+;; If you do clang first, then you get awesome signatures in the
+;; popup for the functions. On the other hand, clang doesn't know
+;; the contenxt and so -> for structs doesn't give completion.
+;; with clang first as well, you can't do C-w to go to definition,
+;; but that's okay since you can do it with semantic anyway with M-]/[
 (setq company-backends '(company-semantic
 			 company-c-headers
 			 company-bbdb
 			 company-ghc))
 
-;;LateX Related Code
-(mapc (lambda (item)
-	(add-to-list 'load-path item))
-      '("~/.emacs.d/el-get/predictive"
-	"~/.emacs.d/el-get/predictive/latex"
-	"~/.emacs.d/el-get/predictive/texinfo"
-	"~/.emacs.d/el-get/predictive/html"))
-
+;; LateX Related Code
 (add-hook 'LaTeX-mode-hook (lambda ()
+			     (mapc (lambda (item)
+				     (add-to-list 'load-path item))
+				   '("~/.emacs.d/el-get/predictive"
+				     "~/.emacs.d/el-get/predictive/latex"
+				     "~/.emacs.d/el-get/predictive/texinfo"
+				     "~/.emacs.d/el-get/predictive/html"))
 			     (predictive-mode)
 			     (flyspell-mode)
 			     (setq TeX-parse-self t)
-			     ;;Since I'm usually just using 1 file
+			     ;; Since I'm usually just using 1 file
 			     (setq-default TeX-master t)
-			     ;;Got tired of hitting m-q
+			     ;; Got tired of hitting m-q
 			     (turn-on-auto-fill)
 			     (LaTeX-math-mode)))
 
-;;Doc-view mode, think viewing pdfs in emacs
+;; Doc-view mode, think viewing pdfs in emacs, pretty robust actually. 
 (add-hook 'doc-view-mode-hook (lambda ()
-				;;Improves resolution at cost of computation
+				;; Improves resolution at cost of computation
 				(setq doc-view-resolution 300)
 				;; Basically poll the file for changes. 
 				(auto-revert-mode)))
 
-;;Python Stuff
+;; Python Stuff
+;; Get these variables set before the inferior mode comes up, otherwise too late.
+(setq python-shell-interpreter "/usr/local/bin/ipython3"
+      python-shell-interpreter-args ""
+      python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+      python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+      python-shell-completion-setup-code
+      "from IPython.core.completerlib import module_completion"
+      python-shell-completion-module-string-code
+      "';'.join(module_completion('''%s'''))\n"
+      python-shell-completion-string-code
+      "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+(add-hook 'inferior-python-mode-hook (lambda ()
+				       (set-process-query-on-exit-flag
+					;; Just like killing the shell without asking me. 
+				       	(get-process "Python") nil)))
+
 (add-hook 'python-mode-hook (lambda ()
 			      (jedi:setup)
 			      (setq jedi:setup-keys t
 				    jedi:complete-on-dot t
 				    jedi:environment-virtualenv '("virtualenv"
 								  "--system-site-packages"
-								   "--quiet"
-								   "--python"
-								   "/usr/local/bin/python3"))
+								  "--quiet"
+								  "--python"
+								  "/usr/local/bin/python3"))
+			      ;; Forgot what this was for..think some os x issues. 
 			      (setenv "LC_CTYPE" "UTF-8")
+			      ;; keeping a consistent interface for autocomplete type things. 
 			      (define-key python-mode-map (kbd "M-/") 'jedi:complete)
 			      (let ((interpreter python-shell-interpreter)
-				    (args python-shell-interpreter-args))
-				(when python-shell--parent-buffer
-				  (python-util-clone-local-variables python-shell--parent-buffer))
-				;; Users can override default values for these vars when calling
-				;; `run-python'. This ensures new values let-bound in
-				;; `python-shell-make-comint' are locally set.
-				(set (make-local-variable 'python-shell-interpreter) interpreter)
-				(set (make-local-variable 'python-shell-interpreter-args) args))
-			      (setq python-shell-interpreter "/usr/local/bin/ipython3"
-				    python-shell-interpreter-args ""
-				    python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-				    python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-				    python-shell-completion-setup-code
-				    "from IPython.core.completerlib import module_completion"
-				    python-shell-completion-module-string-code
-				    "';'.join(module_completion('''%s'''))\n"
-				    python-shell-completion-string-code
-				    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")))
-;;TODO, kill the python-shell-interpreter without bothering me, just like shell, see line 199
+			      	    (args python-shell-interpreter-args))
+			      	(when python-shell--parent-buffer
+			      	  (python-util-clone-local-variables python-shell--parent-buffer))
+			      	;; Users can override default values for these vars when calling
+			      	;; `run-python'. This ensures new values let-bound in
+			      	;; `python-shell-make-comint' are locally set.
+			      	(set (make-local-variable 'python-shell-interpreter) interpreter)
+			      	(set (make-local-variable 'python-shell-interpreter-args) args))))
 
-;;Haskell Stuff
+;; Haskell Stuff
 (add-hook 'haskell-mode-hook (lambda ()
 			       (interactive-haskell-mode)
 			       (ghc-init)
@@ -407,18 +425,19 @@
 ;; 			      (autoload 'merlin-mode "merlin" "Merlin mode" t)
 ;; 			      (merlin-mode)))
 
-;;Orgmode Stuff
+;; Orgmode Stuff
 (add-hook 'org-mode-hook (lambda ()
 			   (flyspell-mode)
 			   (auto-fill-mode)
 			   (define-key org-mode-map (kbd "C-o") 'my-org-defaults)))
-;;Basic text files
+;; Basic text files
 (add-hook 'text-mode-hook 'auto-fill-mode)
 
-;;Debugging Stuff
-;;(setq warning-minimum-log-level "error")
+;; Debugging Stuff
+;; (setq warning-minimum-log-level "error")
+;; Don't really need these, they are more annoying than anything
 (setq make-backup-files nil)
-;;(setq debug-on-error t)
+;; (setq debug-on-error t)
 
 ;;Javascript hook, this is a better major mode than default one
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
@@ -450,7 +469,6 @@
 			  (setq-local show-trailing-whitespace t)
 			  (semantic-mode)
 			  (auto-complete-mode -1)
-			  (abbrev-mode -1)
 			  (company-mode)
 			  (global-set-key (kbd "C-c C-f") 'helm-command-prefix)
 			  (global-unset-key (kbd "C-x c"))
@@ -483,10 +501,12 @@
 			  (define-key c-mode-map (kbd "C-c C-i") 'default-c-includes)
 			  (ggtags-mode)
 			  (define-key ggtags-mode-map (kbd "M-.") nil)
+			  (define-key ggtags-mode-map (kbd "M-<") nil)
+			  (define-key ggtags-mode-map (kbd "M->") nil)
 			  (define-key ggtags-mode-map (kbd "M-,") nil)
 			  (define-key ggtags-mode-map (kbd "M-]") nil)
 			  (define-key ggtags-mode-map (kbd "M-'") 'ggtags-find-reference)
-			  ;;Doesn't really work well over tramp.
-;;			  (flycheck-mode)
+			  ;; Doesn't really work well over tramp, but otherwise its amazing. 
+			  ;; (flycheck-mode)
 			  (linux-c-mode)))
 
