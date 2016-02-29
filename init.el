dfa6aa467b7f9c498e8270860152af88dfa244be
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-02-29 11:39:48 -0800

    Add C++ changes

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 ;; I like it to have emacs open on right half of screen. 
 (when window-system
   (set-frame-position nil 675 0)
-  (set-frame-size nil 85 49))
+  (set-frame-size nil 200 75))
 (global-set-key (kbd "M-n") 'forward-paragraph)
 (global-set-key (kbd "M-p") 'backward-paragraph)
 ;; Not sure why but dialog box still locks up emacs on OSX.
@@ -21,9 +21,8 @@
      (setq initial-scratch-message (buffer-string)))))
 
 (package-initialize)
-(require 'org-page)
 (autoload 'window-number-mode "window-number")
-;; (add-to-list 'company-c-headers-path-system "/usr/local/include/c++/5.2.0")
+
 (autoload 'company-mode "company")
 
 ;;Melpa stuff, elpa is the offical package archive, melpa is the
@@ -40,19 +39,22 @@
  '(column-number-mode t)
  '(custom-safe-themes
    (quote
-    ("d3df47c843c22d8f0177fe3385ae95583dc8811bd6968240f7da42fd9aa51b0b" default)))
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d3df47c843c22d8f0177fe3385ae95583dc8811bd6968240f7da42fd9aa51b0b" default)))
  '(display-battery-mode t)
  '(display-time-default-load-average nil)
  '(display-time-mode t)
- '(flycheck-c/c++-gcc-executable "/usr/local/bin/gcc-4.9")
- '(flycheck-make-executable "/usr/bin/make")
+ '(flycheck-c/c++-clang-executable
+   "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++")
+ '(flycheck-make-executable "/usr/local/bin/make")
+ '(op/theme (quote phaer))
  '(org-startup-indented t)
  '(package-selected-packages
    (quote
-    (nix-mode web-mode org-page objc-font-lock window-number tuareg simple-httpd ox-gfm mustache material-theme js2-mode jade-mode htmlize hlinum flycheck exec-path-from-shell company-tern company-quickhelp company-jedi company-c-headers)))
- '(semantic-c-dependency-system-include-path
-   (quote
-    ("/usr/include" "/usr/local/lib/ocaml" "/usr/local/include" "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS8.3.sdk/System/Library/Frameworks/Foundation.framework/Headers")))
+    (markdown-mode haskell-mode edbi sql-indent sqlup-mode company-shell company-web neotree spacegray-theme solarized-dark-theme ag magit ggtags ido-vertical-mode nix-mode web-mode objc-font-lock window-number tuareg simple-httpd ox-gfm mustache material-theme js2-mode jade-mode htmlize hlinum flycheck exec-path-from-shell company-tern company-quickhelp company-jedi company-c-headers)))
+ '(solarized-distinct-doc-face t)
+ '(solarized-distinct-fringe-background t)
+ '(solarized-high-contrast-mode-line t)
+ '(solarized-use-more-italic t)
  '(tool-bar-mode nil)
  '(web-mode-attr-indent-offset 2))
 
@@ -164,6 +166,7 @@
           (if this-win-2nd (other-window 1))))))
 
 (defun semantic-ia-fast-jump-back ()
+  "Code."
   (interactive)
   (defadvice push-mark
       (around semantic-mru-bookmark activate)
@@ -204,7 +207,7 @@
   (add-hook 'erc-after-connect '(lambda (server nick)
 				  (erc-message
 				   "PRIVMSG"
-1				   (concat "NickServ identify " erc-password))))
+				   1				   (concat "NickServ identify " erc-password))))
   ;; This is what actually does the connection
   (erc :server "irc.freenode.net" :port 6667
        :nick "Algebr" :full-name user-full-name))
@@ -216,6 +219,7 @@
 ;; Love ido, idiot for not using it earlier. 
 (setq ido-everywhere t)
 (ido-mode 1)
+(ido-vertical-mode)
 ;; Use the path set up by zsh, aka the ~/.zshrc. 
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
@@ -230,8 +234,8 @@
 ;; ;; Just kill the shell, don't ask me.
 ;; ;; I do a lambda so that its not evaluated at init load time. 
 (add-hook 'shell-mode-hook (lambda ()
-			     (set-process-query-on-exit-flag
-			      (get-process "shell") nil)))
+			     (company-mode)))
+
 ;; Don't prompt me when I want to clear the buffer
 (put 'erase-buffer 'disabled nil)
 
@@ -239,7 +243,7 @@
 (global-set-key (kbd "s-1") 'neotree-toggle)
 (set-scroll-bar-mode nil)
 ;; ;; Visuals, but note that some visuals also set in custom.
-;; (show-paren-mode)
+(show-paren-mode)
 (auto-insert-mode)
 (abbrev-mode -1)
 (define-auto-insert "\\.org\\'" 'my-org-defaults)
@@ -304,53 +308,30 @@
   (add-hook 'after-init-hook 
 	    (lambda ()
 	      (global-hl-line-mode 1)
-	      (load-theme 'material t))))
+	      (load-theme 'spacegray t))))
 
-;; ;; Semantic Stuff, very important to me, should probably refactor this for
-;; ;; the appropriate modes, eitherwise the globalness of it is annoying when
-;; ;; doing say Python and C, or rather anything else and C. 
-;; (global-semantic-idle-scheduler-mode 1)
-;; (global-semantic-idle-summary-mode 1)
-;; (global-semantic-stickyfunc-mode 1)
-;; (global-semantic-idle-local-symbol-highlight-mode 1)
-;; (global-semantic-mru-bookmark-mode 1)
-;; (global-semanticdb-minor-mode 1)
-;; (global-semantic-decoration-mode 1)
-;; (global-cedet-m3-minor-mode 1)
-;; (semanticdb-enable-gnu-global-databases 'c-mode t)
-;; (global-semantic-show-unmatched-syntax-mode t)
-;; ;; For custom stuff, see this example (Although I skip this crap and just do the stuff
-;; ;; in os-s)
-;; ;; (ede-cpp-root-project "NAME" :file "FILENAME"
-;; ;;     :include-path '( "/include" "../include" "/c/include" )
-;; ;;     :system-include-path '( "/usr/include/c++/3.2.2/" )
-;; ;;     :spp-table '( ("MOOSE" . "")
-;; ;;                   ("CONST" . "const") )
-;; ;;     :spp-files '( "include/config.h" )
-;; ;;     )
-;; ;; If you do clang first, then you get awesome signatures in the
-;; ;; popup for the functions. On the other hand, clang doesn't know
-;; ;; the contenxt and so -> for structs doesn't give completion.
-;; ;; with clang first as well, you can't do C-w to go to definition,
-;; ;; but that's okay since you can do it with semantic anyway with M-]/[
-;; ;; In any case, I prefer using gcc instead of clang, at least for the moment.
-;; ;; the capf, (means completion at point functions), is mainly here for org-mode
-;; (setq
-;;  company-clang-arguments
-;;  '("-F" "/System/Library/Frameworks/Foundation.framework/Versions/C/Headers"
-;;    "-F" "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/Foundation.framework/Headers"))
-(setq company-clang-arguments
-      '("-F" "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/Foundation.framework/Headers/"
-	"-F" "/System/Library/Frameworks/Foundation.framework/Headers/"
-	"-F" "/System/Library/Frameworks/SceneKit.framework/Headers/"
-	"-I" "/usr/local/lib/ocaml/"))
+(setq
+ company-clang-executable
+ "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++"
+ company-clang-arguments
+ '("-std=c++11"
+   "-F" "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/System/Library/Frameworks/Foundation.framework"
+   "-I" "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/JavaScriptCore.framework/Headers"
+   "-F" "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/JavaScriptCore.framework"
+   "-I" "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/usr/include"
+   "-F" "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/System/Library/Frameworks/WebKit.framework"
+   ;; "-F" "/System/Library/Frameworks/Foundation.framework"
+   "-I" "/usr/local/Cellar/folly/0.48.0_1/include"
+   "-I" "/usr/local/include/graphqlparser"
+   "-I" "/usr/local/Cellar/folly/0.48.0_1/include"
+   "-I" "/usr/include/c++/4.2.1"
+   "-I" "/usr/local/lib/ocaml/")
 
-(setq company-backends
-      '(company-clang
-	company-semantic
-	company-c-headers
-	company-capf
-	company-jedi))
+company-backends
+'(company-clang
+  company-capf
+  company-c-headers
+  company-jedi))
 
 ;; ;; LateX Related Code
 ;; (add-hook 'LaTeX-mode-hook (lambda ()
@@ -371,7 +352,7 @@
 
 ;; ;; Python Stuff
 ;; ;; Get these variables set before the inferior mode comes up, otherwise too late.
-(setq python-shell-interpreter "/usr/local/bin/ipython"
+(setq python-shell-interpreter "ipython"
       python-shell-interpreter-args "--matplotlib=osx --colors=Linux"
       python-shell-prompt-regexp "In \\[[0-9]+\\]: "
       python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
@@ -429,11 +410,20 @@
 ;; ;; Just remember,
 ;; ;;http://truongtx.me/2014/08/23/setup-emacs-as-an-sql-database-client/
 ;; ;;(load-file "~/.emacs.d/sql_dbs.el")
-;; (add-hook 'sql-interactive-mode-hook
-;; 	  (lambda ()
-;; 	    (toggle-truncate-lines)))
 
-;; ;; Ocaml code
+(load-file "~/.emacs.d/sql_dbs.el")
+
+(add-hook 'sql-mode
+	  (lambda ()
+	    (sql-highlight-postgres-keywords)
+	    (load-library "sql-indent")
+	    (sqlup-mode)))
+
+(add-hook 'sql-interactive-mode-hook
+	  (lambda ()
+	    (toggle-truncate-lines)))
+
+;; OCaml code
 (add-hook
  'tuareg-mode-hook
  (lambda ()
@@ -455,6 +445,7 @@
    (autoload 'merlin-mode "merlin" "Merlin mode" t)
    (setq-local merlin-completion-with-doc t)
    (utop-minor-mode)
+   (company-quickhelp-mode)
    (setq-local indent-tabs-mode nil)
    (require 'ocp-indent)
    (setq-local indent-line-function 'ocp-indent-line)
@@ -478,7 +469,6 @@
 	    (flyspell-mode)
 	    (auto-fill-mode)
 	    (setq-local org-pretty-entities-include-sub-superscripts t)
-	    (semantic-mode -1)
 	    (company-mode)))
 
 ;; TODO, this shouldn't need to be a separate call, should be
@@ -495,29 +485,33 @@
 (setq make-backup-files nil)
 ;; (setq debug-on-error t)
 
-(add-hook 'html-mode-hook
-	  (lambda ()
-	    (web-mode)
-	    (setq web-mode-ac-sources-alist
-		  '(("css" . (ac-source-css-property))
-		    ("html" . (ac-source-words-in-buffer ac-source-abbrev))))))
+;; (add-hook 'html-mode-hook
+;; 	  (lambda ()
+;; 	    (web-mode)
+;; 	    (setq web-mode-ac-sources-alist
+;; 		  '(("css" . (ac-source-css-property))
+;; 		    ("html" . (ac-source-words-in-buffer ac-source-abbrev))))))
 
 (add-hook 'css-mode-hook (lambda ()
-			   (define-key css-mode-map (kbd "M-/") 'ac-start )))
+			   (define-key css-mode-map (kbd "M-/")
+			     'ac-start )))
 
 (add-hook 'scss-mode-hook (lambda ()
 			    (setq-local scss-compile-at-save t)
 			    (setq-local scss-output-directory "../css")
 			    (auto-complete-mode)
-			    (define-key css-mode-map (kbd "M-/") 'ac-start )))
+			    (define-key css-mode-map (kbd "M-/")
+			      'ac-start )))
 
 ;; ;;Javascript hook, this is a better major mode than default one
-;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-;; (add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
 
-;; (add-hook 'js2-mode-hook (lambda ()
-;; 			   (define-key js2-mode-map (kbd "M-/") 'tern-ac-complete)
-;; 			   (tern-mode)))
+(add-hook 'js2-mode-hook (lambda ()
+			   (company-mode)
+			   (define-key js2-mode-map (kbd "M-/")
+			     'company-tern)
+			   (tern-mode)))
 
 
 ;; (eval-after-load 'tern
@@ -534,7 +528,6 @@
 	  '(lambda ()
 	     (global-set-key (kbd "C-M-s") 'eval-buffer)
 	     (flycheck-mode)
-	     ;;	     (semantic-mode -1)
 	     (company-mode)
 	     (global-unset-key (kbd "C-x c"))))
 
@@ -542,18 +535,35 @@
 (add-hook
  'c-mode-common-hook
  '(lambda ()
-    ;; (load-file "~/.emacs.d/cedet/cedet-devel-load.elc")
-    (semantic-mode)
-    (semantic-decoration-mode)
-    (semantic-show-unmatched-syntax-mode)
-    (semantic-idle-local-symbol-highlight-mode)
-    (semantic-idle-scheduler-mode)
-    (global-semantic-mru-bookmark-mode)
-    (semantic-idle-summary-mode)
-    (semantic-stickyfunc-mode)
-    (semantic-add-system-include "/usr/local/include/c++/5.2.0")
-    (define-key semantic-mode-map (kbd "M-]") 'semantic-ia-fast-jump)
-    (define-key semantic-mode-map (kbd "M-[") 'semantic-ia-fast-jump-back)
+    (ggtags-mode)
+    ;; (semantic-mode)
+    ;; (global-semantic-mru-bookmark-mode)
+    (define-key ggtags-mode-map (kbd "M-.") nil)
+    (define-key ggtags-mode-map (kbd "M-<") nil)
+    (define-key ggtags-mode-map (kbd "M->") nil)
+    (define-key ggtags-mode-map (kbd "M-n") nil)
+    (define-key ggtags-mode-map (kbd "M-p") nil)
+    (add-to-list
+     'company-c-headers-path-system
+     "/Users/Edgar/.opam/working_code/lib/ocaml")
+    (add-to-list
+     'company-c-headers-path-system
+     "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/JavaScriptCore.framework/Headers")
+    (add-to-list
+     'company-c-headers-path-system
+     "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/System/Library/Frameworks/WebKit.framework/Headers")
+    (add-to-list
+     'company-c-headers-path-system
+    "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/Foundation.framework/Headers/")
+    (define-key ggtags-mode-map (kbd "M-,") nil)
+    (define-key ggtags-mode-map (kbd "M-]") nil)
+    ;; (define-key c-mode-map (kbd "C-.") 'ggtags-find-definition)
+    ;; (define-key c-mode-map (kbd "C-,") 'ggtags-prev-mark)
+    ;; (global-unset-key (kbd "C-]"))
+    ;; (define-key c-mode-map (kbd "M-]") 'semantic-ia-fast-jump)
+    ;; (define-key c-mode-map (kbd "M-[") 'semantic-ia-fast-jump-back)
+    ;; (define-key c++-mode-map (kbd "M-]") 'semantic-ia-fast-jump)
+    ;; (define-key c++-mode-map (kbd "M-[") 'semantic-ia-fast-jump-back)
     (define-key c-mode-map (kbd "C-=") 'ff-find-other-file)
     (setq-local show-trailing-whitespace t)
     (company-mode)
@@ -562,55 +572,18 @@
     (flycheck-mode)
     (abbrev-mode -1)))
 
-;; ;; C Code
-;; (add-hook 'c-mode-hook '(lambda ()
-;; 			  (semantic-mode)
-;; 			  (define-key helm-gtags-mode-map (kbd "M-s")
-;; 			    'helm-gtags-select)
-;; 			  (helm-gtags-mode)
-;; 			  (define-key helm-gtags-mode-map (kbd "M-.")
-;; 			    'helm-gtags-dwim)
-;; 			  (define-key helm-gtags-mode-map (kbd "M-,")
-;; 			    'helm-gtags-pop-stack)
-;; 			  (define-key c-mode-map (kbd "C-c C-c") 'compile)
-;; 			  (semantic-mru-bookmark-mode)
-;; 			  (define-key semantic-mode-map (kbd "M-]")
-;; 			    'semantic-ia-fast-jump)
-;; 			  (define-key semantic-mode-map (kbd "M-[")
-;; 			    'semantic-ia-fast-jump-back)
-;; 			  (ggtags-mode)
-;; 			  (define-key ggtags-mode-map (kbd "M-.") nil)
-;; 			  (define-key ggtags-mode-map (kbd "M-<") nil)
-;; 			  (define-key ggtags-mode-map (kbd "M->") nil)
-;; 			  (define-key ggtags-mode-map (kbd "M-n") nil)
-;; 			  (define-key ggtags-mode-map (kbd "M-p") nil)
-;; 			  (define-key ggtags-mode-map (kbd "M-,") nil)
-;; 			  (define-key ggtags-mode-map (kbd "M-]") nil)
-;; 			  (define-key ggtags-mode-map (kbd "M--")
-;; 			    'ggtags-find-reference)))
+(add-hook 'c++-mode-hook
+	  (lambda ()
+	    (setq-local flycheck-clang-language-standard "c++14")
+	    (setq-local company-async-timeout 5)
+	    (setq-local company-async-wait 0.10)
+	    (add-to-list
+	     'company-c-headers-path-system
+	     "/Users/Edgar/.opam/working/lib/ocaml")
+	    (add-to-list 'company-c-headers-path-system
+			 "/usr/local/include/c++/5.3.0")))
 
-;; (defun objc-in-header-file ()
-;;   (let* ((filename (buffer-file-name))
-;;          (extension (car (last (split-string filename "\\.")))))
-;;     (string= "h" extension)))
-
-;; (defun objc-jump-to-extension (extension)
-;;   (let* ((filename (buffer-file-name))
-;;          (file-components (append (butlast (split-string filename
-;;                                                          "\\."))
-;;                                   (list extension))))
-;;     (find-file (mapconcat 'identity file-components "."))))
-
-;; ;;; Assumes that Header and Source file are in same directory
-;; (defun objc-jump-between-header-source ()
-;;   (interactive)
-;;   (if (objc-in-header-file)
-;;       (objc-jump-to-extension "m")
-;;     (objc-jump-to-extension "h")))
-
-;; (defun objc-mode-customizations ()
-;;   (define-key objc-mode-map (kbd "C-c t") 'objc-jump-between-header-source))
-
+(add-to-list 'auto-mode-alist '("\\.xm\\'" . objc-mode))
 
 (add-hook 'objc-mode-hook
 	  '(lambda ()
@@ -618,18 +591,12 @@
 	     (company-mode)))
 
 ;; ;; Configuration for blogging
-(setq
- op/repository-directory "/Users/Edgar/Repos/fxfactorial.github.io/"
- op/site-domain "hyegar.com"
- op/personal-disqus-shortname "hyegar"
- op/site-main-title "Edgar Aroutiounian"
- op/site-sub-title "Thoughts and Musings"
- op/category-ignore-list '("static")
- op/personal-github-link "http://github.com/fxfactorial"
- op/personal-google-analytics-id "UA-57031124-1")
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(add-to-list 'auto-mode-alist '("\\.xm\\'" . objc-mode))
+(add-to-list 'auto-mode-alist '("\\.mm\\'" . objc-mode))
